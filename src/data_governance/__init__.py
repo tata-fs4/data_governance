f917ea680e4e9698240601f9d51037c75a6a04bd
Tales Souza <102629258+tata-fs4@users.noreply.github.com>
2025-10-29 00:11:29 -0300

    Initialize data governance package with modules

---
+++ src/data_governance/__init__.py
@@ -0,0 +1,10 @@
+"""Pacote de governança de dados orientado a compliance."""
+
+__all__ = [
+    "catalog",
+    "access_control",
+    "lineage",
+    "quality",
+    "policies",
+    "pipeline",
+]
